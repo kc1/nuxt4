1e0ad54b267cf39bde1c3ea4bfbc7d134d3f6112
John Doe <john@doe.org>
2018-12-28 15:48:06 -0500

    first commit

---
+++ layouts/default.vue
@@ -0,0 +1,29 @@
+<template>
+<div>
+<Menu/>
+<Carousel/>
+<Footer/>
+</div>
+</template>
+
+<script>
+
+import VuetifyLogo from '~/components/VuetifyLogo.vue'
+import Menu from '~/components/menu.vue'
+import Footer from '~/components/footer.vue'
+import Carousel from '~/components/carousel.vue'
+
+export default {
+components: {
+  VuetifyLogo,
+  Carousel,
+  Footer,
+  Menu
+}
+}
+
+</script>
+
+<style>
+
+</style>
